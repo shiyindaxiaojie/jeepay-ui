9e1662ca2699b5d9cea04b891e6b9f640c656645
shiyindaxiaojie <1813986321@qq.com>
2023-02-04 23:49:31 +0800

    update

--- jeepay-ui-manager/src/components/GlobalFooter/index.vue
+++ jeepay-ui-manager/src/components/GlobalFooter/index.vue
@@ -3,7 +3,7 @@
     <template v-slot:links>
     </template>
     <template v-slot:copyright>
-      Copyright © 2022 <a href="http://www.jeequan.com" target="_blank">jeequan.com</a>. All rights reserved.
+      Copyright © 2022 <a href="https://github.com/shiyindaxiaojie" target="_blank">梦想歌</a>. All rights reserved.
     </template>
   </global-footer>
 </template>
